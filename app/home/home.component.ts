c5f29ee0c0607829b1095d9dbe914c5baf354ee6
Ujjal Kumar Dutta <duttaujjal143@gmail.com>
2018-03-04 03:11:08 +0530

    Couchdb test complete

--- app/home/home.component.ts
+++ app/home/home.component.ts
@@ -8,6 +8,8 @@ import { FireBaseDbService } from "../services/fire-base-db.service";
 
 import { QrcodeService } from "../services/qrcode.service"
 
+const couchbaseModule = require("nativescript-couchbase");
+
 @Component({
     selector: "Home",
     moduleId: module.id,
@@ -17,11 +19,25 @@ export class HomeComponent implements OnInit {
 
     public data ="Fb Data";
     public imgSource = "";
+    public database;
+
+    public documentId;
+
     constructor(private firebase : FireBaseDbService,private barcodeService : QrcodeService) {
+        this.database = new couchbaseModule.Couchbase("test-database");
     }
 
     ngOnInit(): void {
-       
+        this.documentId = this.database.createDocument({
+            "firstname": "Nic",
+            "lastname": "Raboy",
+            "address": {
+                "city": "San Francisco",
+                "state": "CA",
+                "country": "USA"
+            },
+            "twitter": "https://www.twitter.com/nraboy"
+        });
     }
 
     check(){
@@ -38,10 +54,19 @@ export class HomeComponent implements OnInit {
     }
 
     scan(){
-        this.barcodeService.scanBarcode();
+        //this.barcodeService.scanBarcode();
         // this.imgSource = this.barcodeService.generateBarcode({
         //     name : 'Ujjal',
         //     uuid : '1234#abcd'
         // });
+
+        console.log("DocID : "+JSON.stringify(this.documentId));
+
+        var person = this.database.getDocument(this.documentId);
+        console.log("Data :"+JSON.stringify(person));
     }
 }
+/**
+ * For Android XML error : remove and rebuild
+ * for : dexoverflow : see app.gradle in app_resource
+ */
